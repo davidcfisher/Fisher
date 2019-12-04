e547b72a835c666330e862bf7da78c9b1de6adf3
Dave Fisher <github@jadco.com>
2019-12-04 10:27:42 -0500

    Add-a-return branch noted

--- SDCT/SDCT.ino
+++ SDCT/SDCT.ino
@@ -14,6 +14,8 @@ void setup()
   mayflyCard.redLED.turnOff(debugFlag);               // turn off the red LED
   
   mayflyCard.SdCard.TIA_dir(debugFlag);               // list the SD Card directories & files
+
+SerialMon.println("This is the Add-a-return branch.");  
   
   if (debugFlag) { SerialMon.print(__FILE__);SerialMon.print(", line ");SerialMon.print(__LINE__); SerialMon.println(": exiting Sketch setup()."); }
 }
