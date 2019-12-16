46c6a1bd8ff40c49776b97dc22e00fdf1aa2600c
Dave Fisher <github@jadco.com>
2019-12-16 08:15:35 -0500

    Refine SDCT.ino

--- SDCT/SDCT.ino
+++ SDCT/SDCT.ino
@@ -11,15 +11,15 @@ void setup()
   delay(1000);                                                    // wait a couple of seconds
   mayflyCard.redLED.turnOff();                                    // turn off the red LED
 
-  // get the directory from the SD Card
-  const int sdCardDirectoryLimit = 5;                            // limit the number of directory names + file names to be displayed
-  SdCardDirectory sd_card_directory[sdCardDirectoryLimit];       // define an array to hold the SD Card directory results
-  int numberOfEntries = mayflyCard.SdCard.TIA_dir(&sd_card_directory[0], sdCardDirectoryLimit);     // get the SD Card directory & file names
-
   // get the console information
   const int consoleRecordLimit = 200;
   consoleRecord console_record[consoleRecordLimit];
   int numberOfConsoleRecords = mayflyCard.SdCard.TIA_consoleRead(&console_record[0], "2019-09-01 11:30:00", "2019-09-01 13:29:00", consoleRecordLimit);
+
+  // get the directory from the SD Card
+  const int sdCardDirectoryLimit = 5;                            // limit the number of directory names + file names to be displayed
+  SdCardDirectory sd_card_directory[sdCardDirectoryLimit];       // define an array to hold the SD Card directory results
+  int numberOfEntries = mayflyCard.SdCard.TIA_dir(&sd_card_directory[0], sdCardDirectoryLimit);     // get the SD Card directory & file names
 
 
   /***** the code below displays the console record information *****/
